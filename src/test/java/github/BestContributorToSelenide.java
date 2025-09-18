29cce20ca67f61cedd0916986c111ef8f1c5a572
Olga Popova <p.olgapopova@mail.ru>
2025-09-18 15:27:21 +0300

    Initial commit

---
+++ src/test/java/github/BestContributorToSelenide.java
@@ -0,0 +1,25 @@
+package github;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class BestContributorToSelenide {
+
+    @Test
+    void andreiSolntsevShouldBeTheFirstContributor() {
+        // открыть страницу репозитория селенида
+        Selenide.open("https://github.com/selenide/selenide");
+        // подвести мышку к первому аватару из блока contributors
+        $("div.Layout-sidebar").$(byText("Contributors"))
+                //.closest(".BorderGrid-cell").$$("ul li").first().hover();
+                .closest("h2").sibling(0).$$("li").first().hover();
+        // проверка: во всплывающем окне есть текст Andrei Solntsev
+        $(".Popover-message").shouldHave(text("Andrei Solntsev"));
+        sleep(5000);
+
+    }
+}
